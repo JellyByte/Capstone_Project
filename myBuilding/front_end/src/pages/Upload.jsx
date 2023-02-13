df22de0e866a4cae0126ebd4567ca51d10fd3e0a
JellyByte <joeyeung100@gmail.com>
2023-02-13 13:34:42 -0500

    Adding page to test uploading

---
+++ myBuilding/front_end/src/pages/Upload.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+export const Upload = () => {
+  return (
+    <div>
+      <div>
+        <h2>
+
+
+          Hello
+        </h2>
+
+
+      </div>
+      <div>
+        <h1>
+          another Hello!
+        </h1>
+      </div>
+      
+      </div>
+  )
+}
