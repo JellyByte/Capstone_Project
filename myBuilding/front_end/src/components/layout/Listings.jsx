ad28b3a6290cbbc2ca95ed83bb9fe32dcfe97e3d
afreenahmad <afreenahmad01@gmail.com>
2023-03-20 17:44:39 -0400

    ok

--- myBuilding/front_end/src/components/layout/Listings.jsx
+++ myBuilding/front_end/src/components/layout/Listings.jsx
@@ -3,6 +3,7 @@ import UploadForm from '../common/UploadForm';
 
 import { SingleListing } from '../LandLord/SingleListing';
 import ImageGrid from './ImageGrid';
+import { ListingsSearchBar } from './ListingsSearchBar';
 
 export const Listings = () => {
 
@@ -11,6 +12,7 @@ export const Listings = () => {
   
   return (
     <div className="flex flex-col gap-2 p-2 ">
+      <ListingsSearchBar/>
       <ImageGrid/>
 
 
