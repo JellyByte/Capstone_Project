28db4504a62799fbc338803f58d1ae3c2ed2b864
afreenahmad <afreenahmad01@gmail.com>
2023-03-27 12:25:34 -0400

    f

--- myBuilding/front_end/src/components/layout/ListingsSearchBar.jsx
+++ myBuilding/front_end/src/components/layout/ListingsSearchBar.jsx
@@ -8,7 +8,7 @@ export const ListingsSearchBar= () => {
             <input 
             type="text" 
             className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
-            placeholder="Search" />
+            placeholder="Search something" />
         </div>
     )
 }
