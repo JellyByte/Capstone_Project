4b0b663f2bd04d71f0b10afead62a7713be2cf48
afreenahmad <afreenahmad01@gmail.com>
2023-04-06 16:10:41 -0400

    editing lisitng

--- myBuilding/front_end/src/components/LandLord/SingleListing.jsx
+++ myBuilding/front_end/src/components/LandLord/SingleListing.jsx
@@ -1,16 +1,36 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase-config";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+
 export const SingleListing = (props) => {
-  console.log(props.descrip);
+  const [showBigImage, setShowBigImage] = useState(false);
+
+  const handleClick = () => {
+    setShowBigImage(true);
+  };
+
+  const handleClose = () => {
+    setShowBigImage(false);
+  };
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg border-2 border-gray-300 hover:shadow-xl transition-shadow duration-300">
       <img
         className="w-full h-64 object-cover object-center hover:opacity-75 transition-opacity"
         src={props.photoURL}
         alt={props.title}
+        onClick={handleClick}
       />
+      {showBigImage && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center"
+          onClick={handleClose}
+        >
+          <img
+            className="max-h-full max-w-full"
+            src={props.photoURL}
+            alt={props.title}
+          />
+        </div>
+      )}
       <div className="px-6 py-4 bg-white">
         <div className="font-bold text-xl mb-2">{props.title}</div>
         <p className="text-gray-700 text-base mb-2">
