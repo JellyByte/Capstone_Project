4734f6103cd17c0b506a0f61efa6e28994360425
vy2000eng <vy2000eng@gmail.com>
2023-04-20 00:03:47 -0400

    made modal stand out

--- myBuilding/front_end/src/components/common/AddTenantModal.jsx
+++ myBuilding/front_end/src/components/common/AddTenantModal.jsx
@@ -16,13 +16,8 @@ import { doc, updateDoc } from "firebase/firestore";
 import { updateProfile } from "firebase/auth";
 import { getMetadata } from "firebase/storage";
 import { Link } from "react-router-dom";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
-import { auth} from "../../firebase-config";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { auth } from "../../firebase-config";
 
 export default function AddTenantModal(props) {
   const [showModal, setShowModal] = React.useState(false);
@@ -40,16 +35,14 @@ export default function AddTenantModal(props) {
     setSearchQuery("");
   };
 
-  const  handleClick= async ()=>{
-
-    setShowModal(true)
-    const usersRef = collection(db, "users")
-    
-
-
-
-
-  }
+  const handleClick = async () => {
+    setShowModal(true);
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.data());
+    });
+  };
 
   return (
     <div>
@@ -61,9 +54,11 @@ export default function AddTenantModal(props) {
         Add a tenant
       </button>
       {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-          <div className="bg-white rounded-lg p-8">
-            <h2 className="text-2xl font-bold mb-4">Add Tenant</h2>
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg p-8 shadow-lg">
+            <h2 className="text-2xl font-bold mb-4 text-gray-900">
+              Add Tenant
+            </h2>
             <form onSubmit={handleSubmit}>
               <label className="block mb-4">
                 <span className="text-gray-700 font-bold">Search:</span>
@@ -77,20 +72,20 @@ export default function AddTenantModal(props) {
               <div className="flex justify-end">
                 <button
                   type="button"
-                  className="bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg mr-4"
+                  className="bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg mr-4 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
                   onClick={handleAdd}
                 >
                   Add
                 </button>
                 <button
                   type="submit"
-                  className="bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg"
+                  className="bg-emerald-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
                 >
                   Submit
                 </button>
                 <button
                   type="button"
-                  className="ml-4 text-gray-700 hover:text-gray-900"
+                  className="ml-4 text-gray-700 hover:text-gray-900 focus:outline-none"
                   onClick={() => setShowModal(false)}
                 >
                   Cancel
@@ -103,4 +98,3 @@ export default function AddTenantModal(props) {
     </div>
   );
 }
-
